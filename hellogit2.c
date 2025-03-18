b01e0096387e280625628470a525f9ee303f66dd
laubrap <brunapreal@gmail.com>
2025-03-18 18:37:57 -0300

    correccion conflicto

--- hellogit2.c
+++ hellogit2.c
@@ -2,12 +2,8 @@
 
 int main()
 {
-<<<<<<< HEAD
     printf("san lorenzo pe version numero 3 fachera");
-=======
-    printf("san lorenzo pe V.3");
->>>>>>> main
     printf("aguante lanu");
     printf("arriba argentinos");
     return 0;
-}
+}   
