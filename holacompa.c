885f76c89aae6a62c54ec388cf24322f4aa9b630
laubrap <brunapreal@gmail.com>
2025-03-19 18:18:24 -0300

    se agrega hola compa

---
+++ holacompa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[40],pasword[20];
+
+    scanf("%99s",name);
+    scanf("%99s",pasword);
+    
+    printf("esta es mi edicion");
+
+    return 0;
+}
